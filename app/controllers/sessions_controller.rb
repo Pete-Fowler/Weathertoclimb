e82161c5f95aee44a533611bdd708b7e1420e239
Pete Fowler <fowler.pete@gmail.com>
2022-10-31 14:51:43 -0600

    Add sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,24 @@
 class SessionsController < ApplicationController
-  
+  # skip_before_action :authorize, only: [:create]
+
   def create 
     user = User.find_by(username: params[:username])
-    session[:user_id] = user.id 
-    render json: user, status: :accepted
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id 
+      render json: user, status: :created
+    else 
+      render json: { error: 'Invalid username or password' }, status: :unauthorized
+    end
   end 
 
   def destroy = User.find_by(username: params[:username])
     session.delete :user_id
     render json: {}, status: :accepted
+  end
+
+  private
+
+  def user_params 
+    params.permit(:username, :password, :password_confirmation)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize, only: [:show]
+  
   def show 
     user = User.find_by(id: session[:user_id])
     if user 
       render json: user
     else 
       render json: {error: 'Not authorized'}, status: :unauthorized
+  end 
+
+  def create 
+    user = User.create!(user_params)
+    
   end 
 
 end
