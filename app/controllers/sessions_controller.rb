6eb9d51d0d3517ca28a314a1cad16b7d9a759c82
Pete Fowler <fowler.pete@gmail.com>
2022-10-31 14:36:58 -0600

    Add /logout and sessions#destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,12 @@
 class SessionsController < ApplicationController
   
+  def create 
+    user = User.find_by(username: params[:username])
+    session[:user_id] = user.id 
+    render json: user, status: :accepted
+  end 
+
+  def destroy = User.find_by(username: params[:username])
+    session.delete :user_id
+    render json: {}, status: :accepted
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :users
   
   post '/login', to: 'sessions#create'
-  
+  delete '/logout', to: 'sessions#destroy'
 end
