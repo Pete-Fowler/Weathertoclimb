4116689dffc0ca01f5203e3fc51f6b92e19f170f
Pete Fowler <fowler.pete@gmail.com>
2022-10-31 14:55:57 -0600

    Add rescue_from invalid to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+
   before_action :authorize
 
   private 
 
   def authorize
     render json: {error: 'Not authorized'}, status: :unauthorized unless session.include? :user_id
+  end
+
+  def render_invalid(exception)
+    render json: { exception.record.errors.full_messages }, status: :unprocessable_entity
   end
 end
