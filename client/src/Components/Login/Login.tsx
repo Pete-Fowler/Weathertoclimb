1397f67a2c7f9eba5e134c2d3c186305f1918382
Pete Fowler <fowler.pete@gmail.com>
2022-11-01 11:49:39 -0600

    Add styles.d.ts

--- client/src/Components/Login/Login.tsx
+++ client/src/Components/Login/Login.tsx
@@ -1,15 +1,12 @@
 import { Link } from 'react-router-dom';
+import style from './Login.module.css';
 
 export default function Login({ user, onChangeUser }) {
 
   return <div>
-    <form>
-      <label>
-        <input name='username'></input>
-      </label>
-      <label>
-        <input name='password'></input>
-      </label>
+    <form className={style.login}>
+      <div><label>Username: <input name='username'></input></label></div>
+      <div><label>Password: <input name='password'></input></label></div>
     </form>
     <Link to='create-account'>Create an account</Link>
   </div>
