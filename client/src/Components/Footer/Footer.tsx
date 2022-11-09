e49e66c15522d12a10d8b553d30c646fdc2f5835
Pete Fowler <fowler.pete@gmail.com>
2022-11-09 13:05:38 -0700

    Add mountains svg to footer

--- client/src/Components/Footer/Footer.tsx
+++ client/src/Components/Footer/Footer.tsx
@@ -1,8 +1,13 @@
 import style from './Footer.module.css';
+import mountains from './mountains.svg';
 
 export default function Footer() {
 
   return <div className={style.footer}>
-    This is the footer
-  </div>
+    <img src={mountains} alt={'Mountains'}/>
+    <ul>
+      <li>Powered by the <a className='link' href="https://www.weather.gov/documentation/services-web-api" title="National Weather Service">National Weather Service API</a></li>
+      <li>Map location icons created by IconMarketPK - <a className='link' href="https://www.flaticon.com/free-icons/location" title="location icons">Flaticon</a></li>
+    </ul>
+</div>
 }
