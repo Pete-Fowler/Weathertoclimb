c03f4b1e1863d63cc9128b7dc1d35d1e80932f17
Pete Fowler <fowler.pete@gmail.com>
2022-11-09 09:31:24 -0700

    Add pin .png to map markers

--- client/src/Components/Map/Map.tsx
+++ client/src/Components/Map/Map.tsx
@@ -1,6 +1,5 @@
 import GoogleMapReact from 'google-map-react';
 import style from './Map.module.css';
-import Marker from './Marker';
 
 interface Iprops {
   center: {lat: number, lng: number},
@@ -17,13 +16,7 @@ export default function Map({center, zoom, markers}: Iprops){
         defaultCenter={center}
         defaultZoom={zoom}
       >
-        {markers
-        // <Marker
-        //   lat={39.710506}
-        //   lng={-105.120089}
-        //   text="My Marker"
-        // />
-        }
+        {markers}
       </GoogleMapReact>
     </div>
   );
--- client/src/Components/Map/Marker.tsx
+++ client/src/Components/Map/Marker.tsx
@@ -1,7 +1,10 @@
-
+import pin from './pin.png';
+import style from './Marker.module.css';
 
 export default function Marker ({ name }: any) {
 
-return <div>{name}</div>;
+return <div className={style.marker}>
+  <img src={pin} alt='marker'/>
+  </div>
 
 }
