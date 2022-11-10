f727cf84cb27be602e31d8fd564abb4ae66cef3b
Pete Fowler <fowler.pete@gmail.com>
2022-11-10 15:36:58 -0700

    Add refetch on error to Details

--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import style from './Header.module.css';
 import { Link } from 'react-router-dom';
-import { ChangeEvent, useState, useEffect } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 interface Props {
   user: {
