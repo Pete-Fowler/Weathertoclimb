a1487c35e34df2bd2a9e70b45893a48abe5c9308
Pete Fowler <fowler.pete@gmail.com>
2022-11-09 19:08:42 -0700

    Remove console.log

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -45,7 +45,6 @@ export default function App() {
 
   function changeModal(str: string) {
     setModal(str);
-    console.log('model set to ' + str);
   }
 
   const isHidden = modal === '' ? 'hidden' : '';
