37573607d4fd2ece585df661f25558c263c6481f
Pete Fowler <fowler.pete@gmail.com>
2022-11-11 10:47:26 -0700

    Add backend validation for 15 favorites max

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,16 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :location
-  validates :user, presence: true
-  validates :location, presence: true 
+
+  validates :user_id, presence: true, uniqueness: { scope: :location_id }
+  validates :location_id, presence: true 
+
+  validate :max_entries
+
+  def max_entries 
+    if Favorite.where(user_id: user_id).length > 5 
+      errors.add(:user_id, :exceeded_15, message: 'Maximum saved areas is 15')
+    end
+  end
+
 end
