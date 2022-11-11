ef4bf503890f128ab14c7822dec05d885762d173
Pete Fowler <fowler.pete@gmail.com>
2022-11-11 10:19:13 -0700

    Add username backend validation

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :location
   validates :user, presence: true
-  validates :location, presence: true
+  validates :location, presence: true 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :username, presence: :true, numericality: { in: 3..30 }
+
   has_many :favorites
   has_many :locations, through: :favorites
   
