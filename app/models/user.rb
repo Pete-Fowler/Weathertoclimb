125301f4f9988d3766de8077c6fdc9958daf2a55
Pete Fowler <fowler.pete@gmail.com>
2022-11-15 10:15:04 -0700

    Seed admin user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :username, presence: :true, numericality: { in: 3..30 }
+  validates :username, presence: :true, length: { in: 3..30 }
+  validates :password, length: { in: 6..16 }
 
   has_many :favorites
   has_many :locations, through: :favorites
